f2d8533f0a143c93e4e26cd516b6b04914d39c35
Francesco Losciale <francesco.losciale@thoughtworks.com>
2020-11-08 17:31:28 +0000

    async lib

--- src/clj_async_tea_party/core.clj
+++ src/clj_async_tea_party/core.clj
@@ -1,6 +1,37 @@
-(ns clj-async-tea-party.core)
+(ns clj-async-tea-party.core
+  (:require [clojure.core.async :as async]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def tea-channel (async/chan))
+
+;; synchronously
+
+;;; getting on a value in `tea-channel` would break the thread (since we are going to use synchronous api)
+;;; with buffered channels the thread is stopped only when the buffer gets full
+(def tea-channel-with-buffer (async/chan 10))
+
+;;; !! means a blocking call
+;;; blocking put
+(async/>!! tea-channel-with-buffer :cup-of-tea)
+;;; blocking get, that drains the channel
+(async/<!! tea-channel-with-buffer)
+
+;;; close channel to new inputs (afterwards you can still get off values)
+(async/close! tea-channel-with-buffer)
+
+; asynchronously
+;;; everything in a `go` block has its own special pool of threads.
+;;; a take from the channel which would normally block, will only pause the execution instead.
+
+(let [tea-channel (async/chan)]
+  (async/go (async/>! tea-channel :cup-of-tea-1))
+  (async/go (println "Thanks for the " (async/<! tea-channel))))
+
+(def tea-channel (async/chan 10))
+(async/go-loop []
+  (println "Thanks for the " (async/<! tea-channel))
+  (recur))
+
+(async/>!! tea-channel :hot-cup-of-tea)
+(async/>!! tea-channel :hot-with-sugar)
+(async/>!! tea-channel :hot-with-milk)
+
