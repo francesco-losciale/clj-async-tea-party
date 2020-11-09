36cb324a0057ac29e93e001b5963bce379ecae30
Francesco Losciale <francesco.losciale@thoughtworks.com>
2020-11-09 17:31:07 +0000

    request tea to google and yahoo and println the first that arrives

--- src/clj_async_tea_party/core.clj
+++ src/clj_async_tea_party/core.clj
@@ -1,52 +1,32 @@
 (ns clj-async-tea-party.core
   (:require [clojure.core.async :as async]))
 
-(def tea-channel (async/chan))
+(def google-tea-service-chan (async/chan 10))
+(def yahoo-tea-service-chan (async/chan 10))
 
-;; synchronously
+(defn random-add []
+  (reduce + (conj [] (repeat (rand-int 100000) 1))))
 
-;;; getting on a value in `tea-channel` would break the thread (since we are going to use synchronous api)
-;;; with buffered channels the thread is stopped only when the buffer gets full
-(def tea-channel-with-buffer (async/chan 10))
+(defn request-google-tea-service []
+  (async/go
+    (random-add)
+    (async/>! google-tea-service-chan
+              "tea compliments of google")))
 
-;;; !! means a blocking call
-;;; blocking put
-(async/>!! tea-channel-with-buffer :cup-of-tea)
-;;; blocking get, that drains the channel
-(async/<!! tea-channel-with-buffer)
+(defn request-yahoo-tea-service []
+  (async/go
+    (random-add)
+    (async/>! yahoo-tea-service-chan
+              "tea compliments of yahoo")))
 
-;;; close channel to new inputs (afterwards you can still get off values)
-(async/close! tea-channel-with-buffer)
+(defn request-tea []
+  (request-google-tea-service)
+  (request-yahoo-tea-service)
+  (async/go (let [[v] (async/alts!
+                        [google-tea-service-chan
+                         yahoo-tea-service-chan])]
+              (println v))))
 
-; asynchronously
-;;; everything in a `go` block has its own special pool of threads.
-;;; a take from the channel which would normally block, will only pause the execution instead.
 
-(let [tea-channel (async/chan)]
-  (async/go (async/>! tea-channel :cup-of-tea-1))
-  (async/go (println "Thanks for the " (async/<! tea-channel))))
 
-(def tea-channel (async/chan 10))
-(async/go-loop []
-  (println "Thanks for the " (async/<! tea-channel))
-  (recur))
 
-(async/>!! tea-channel :hot-cup-of-tea)
-(async/>!! tea-channel :hot-with-sugar)
-(async/>!! tea-channel :hot-with-milk)
-
-; reading multiple channels
-(def tea-channel (async/chan 10))
-(def milk-channel (async/chan 10))
-(def sugar-channel (async/chan 10))
-
-(async/go-loop []
-  (let [[v ch]
-        (async/alts! [tea-channel
-                      milk-channel
-                      sugar-channel])]
-    (println "Got " v " from " ch)
-    (recur)))
-
-(async/>!! sugar-channel :sugar)
-(async/>!! milk-channel :sugar)
